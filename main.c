ca12be1cd297380941b45f2f2d459623c968b3a3
Talita L. Lima <ludmilalima@outlook.com>
2025-05-02 12:27:11 +0000

    Remove reverse string function and related print statements from main.c

--- main.c
+++ main.c
@@ -1,28 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-char* reverseString(char* s, int sSize) {
-    int halfSize = sSize / 2;
-    int forwardsPointer = 0;
-    int backwardsPointer = sSize - 1;
-    for (int i = 0; i < halfSize; i++) {
-        char front = s[forwardsPointer];
-        s[forwardsPointer] = s[backwardsPointer];
-        s[backwardsPointer] = front;
-        forwardsPointer++;
-        backwardsPointer--;
-    }
-    return s;
-}
-
 int main() {
     // Declare the string as a writable array
     char str[] = "Hello, World!!";
     int strSize = strlen(str);
-
-    printf("Original String: %s\n", str);
-    reverseString(str, strSize);
-    printf("Reversed String: %s\n", str);
 
     return 0;
 }
